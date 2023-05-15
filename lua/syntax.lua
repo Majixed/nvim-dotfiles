ccc3d06d1a6e6aededbc6f39208cb8cadc71d1c3
Majixed <gmesashley6@gmail.com>
2023-05-15 12:43:55 +0400

    treesitter - ensure these languages are installed

--- lua/syntax.lua
+++ lua/syntax.lua
@@ -1,6 +1,22 @@
 local treesitter = require('nvim-treesitter.configs')
 
 local config = {
+  ensure_installed = {
+    'vim',
+    'vimdoc',
+    'lua',
+    'luadoc',
+    'bash',
+    'diff',
+    'gitignore',
+    'gitcommit',
+    'python',
+    'latex',
+    'markdown',
+    'rst',
+    'json',
+  },
+  auto_install = true,
   highlight = {
     enable = true,
   },
