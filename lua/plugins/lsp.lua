da0510dfb19cccb322ce5dfae68b9115bf796f20
Majixed <gmesashley6@gmail.com>
2023-05-15 20:40:28 +0400

    Use neodev, set nvim-cmp mappings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,32 +8,69 @@ return {
     'hrsh7th/nvim-cmp',
     'hrsh7th/cmp-vsnip',
     'hrsh7th/vim-vsnip',
+    'folke/neodev.nvim'
   },
   config = function()
     local lspconfig = require('lspconfig')
+    local neodev = require('neodev')
     local cmp = require('cmp')
 
-    cmp.setup({
+    neodev.setup {}
+
+    cmp.setup {
       snippet = {
         expand = function(args)
-          vim.fn["vsnip#anonymous"](args.body)
+          vim.fn['vsnip#anonymous'](args.body)
         end,
       },
-      mapping = cmp.mapping.preset.insert({
+      mapping = cmp.mapping.preset.insert {
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-      }),
-      sources = cmp.config.sources({
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+
+        ['<Tab>'] = cmp.mapping(function(fallback)
+			    if cmp.visible() then
+				    cmp.select_next_item()
+			    else
+				    fallback()
+			    end
+		    end, {'i', 's'}),
+
+		    ['<S-Tab>'] = cmp.mapping(function(fallback)
+			    if cmp.visible() then
+				    cmp.select_prev_item()
+			    else
+				    fallback()
+			    end
+		    end, {'i', 's'}),
+
+        ['<Down>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.close()
+            fallback()
+          else
+            fallback()
+          end
+        end, { 'i' }),
+
+        ['<Up>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.close()
+            fallback()
+          else
+            fallback()
+          end
+        end, { 'i' })
+      },
+      sources = cmp.config.sources {
         { name = 'nvim_lsp' },
         { name = 'vsnip' },
         { name = 'buffer' },
         { name = 'path' },
-        { name = 'cmdline' }
-      })
-    })
+      }
+    }
 
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
@@ -43,14 +80,11 @@ return {
           runtime = {
             version = 'LuaJIT',
           },
-          diagnostics = {
-            globals = {'vim'},
+          completion = {
+            callSnippet = 'Replace',
           },
           workspace = {
-            library = vim.api.nvim_get_runtime_file('', true),
-          },
-          telemetry = {
-            enable = false,
+            checkThirdParty = false,
           },
         },
       },
