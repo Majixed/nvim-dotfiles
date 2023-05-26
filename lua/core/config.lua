d166e2281869ac400e981a5381ae77cf5778d824
Majixed <gmesashley6@gmail.com>
2023-05-26 20:55:41 +0400

    configure nvim-tree

--- lua/core/config.lua
+++ lua/core/config.lua
@@ -6,9 +6,6 @@ vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 vim.o.showmode = false
 vim.o.wrap = false
 vim.o.number = true
--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -2,19 +2,23 @@ return {
   'nvim-tree/nvim-tree.lua',
   dependencies = 'nvim-web-devicons',
   config = function()
-    require('nvim-tree').setup {}
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
 
-    local function open_nvim_tree(data)
-      local directory = vim.fn.isdirectory(data.file) == 1
-
-      if not directory then
-        return
-      end
-
-      vim.cmd.cd(data.file)
-      require('nvim-tree.api').tree.open()
-    end
-
-    vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
+    require('nvim-tree').setup {
+      hijack_cursor = true,
+      disable_netrw = true,
+      hijack_netrw = true,
+      renderer = {
+        indent_markers = {
+          enable = true
+        },
+        icons = {
+          show = {
+            folder_arrow = false
+          }
+        }
+      }
+    }
   end
 }
